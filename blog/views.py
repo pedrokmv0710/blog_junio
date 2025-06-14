0f0786b264c34d3659fe2df95953fe0f1233b68e
pedrokmv0710 <keikoalberto36@gmail.com>
2025-06-14 17:49:23 -0400

    lista de publicaciones

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post-list.html'  # Specify your template name here
+
+    def get_queryset(self):
+        return Post.objects.all().order_by('-created_at')  # Order posts by creation date, newest first
