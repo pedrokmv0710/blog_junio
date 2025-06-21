a1c54a3aabaa010ff46d8be686eab5582988b549
pedrokmv0710 <keikoalberto36@gmail.com>
2025-06-21 16:37:28 -0400

    post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
+
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),  # URL for the post list view
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
-    template_name = 'post-list.html'  # Specify your template name here
+    template_name = 'post-list.html' 
+    
+#details view
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post-detail.html'
 
-    def get_queryset(self):
-        return Post.objects.all().order_by('-created_at')  # Order posts by creation date, newest first
