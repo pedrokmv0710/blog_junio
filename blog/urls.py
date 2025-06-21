82731710c8cedeafc452d29a2bd94b3ae59c2b0d
pedrokmv0710 <keikoalberto36@gmail.com>
2025-06-21 16:57:25 -0400

    post-detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from .views import PostListView, PostDetailView
 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'), 
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), 
 ]
